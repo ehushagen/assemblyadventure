604b01e6027e26a51801bb2e92856529a61eed14
ehushagen <eric.hushagen@gmail.com>
2020-08-11 11:55:40 -0600

    .

--- lesson02/ExtraLifeWithLabels.asm
+++ lesson02/ExtraLifeWithLabels.asm
@@ -9,12 +9,15 @@
 
 ; as before, we will use memory locations $00:0000 and $00:0001 to store
 ;	the number of coins and lives
+
 	lda #$00			; load the A register with zero...
 	sta coins		; ...and store in memory
 	lda #$03			; load the A register with three...
 	sta lives		; and store in memory
 
+;////////////////////////////////
 ; imaginary game code goes here
+;////////////////////////////////
 
 ; check the number of coins, presumably after picking one up in-game
 	lda coins
@@ -28,4 +31,6 @@
 	sta lives
 Done:
 
+;//////////////////////////////////////
 ; more imaginary game code continues
+;//////////////////////////////////////
